d86b1e89b705de73fd2a9a86309453701d45ee89
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-13 00:59:04 +0200

    648. Score Keeper Exercise Colt Solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Counter from './Counter'
 import Scorekeeper from './Scorekeeper'
 import EmojisClicker from './EmojisClicker'
 import ScoreKeeperExercise from './ScoreKeeperExercise'
+import ScoreKeeperColt from './ScoreKeeperColt'
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
      {/*<Counter/>*/}
       {/*<Scorekeeper/>*/}
       {/*<EmojisClicker/>*/}
-      <ScoreKeeperExercise numPlayers={5} target={3}/>
+      {/*<ScoreKeeperExercise numPlayers={5} target={3}/>*/}
+      <ScoreKeeperColt/>
     </>
   )
 }
